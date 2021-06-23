277fbce6a34dd901214763166d185fc986a97e61
Sebastian Remm <sebastian.remm@web.de>
2021-06-23 20:24:46 +0200

    First steps for the home page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,23 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet" />
       </Head>
-      <section className={home.test}/>
+      <section className={home['top-container']}>
+        <div className={home['picture-container']}>
+          <Image
+            src="/images/profile.jpg"
+            alt="Profile picture Sebastian Remm"
+            className={home.picture}
+            width="30%"
+            height="auto"
+            layout="responsive"
+            priority="true"
+            placeholder="blur"
+          />
+        </div>
+        <span className={home['top-description']}>
+
+        </span>
+      </section>
     </Layout>
   )
 }
