82d056c6efb1ad8f18f4b8015110a8d006aab407
Sebastian Remm <sebastian.remm@web.de>
2021-06-24 20:46:41 +0200

    no if/else magic anymore

--- pages/cv.js
+++ pages/cv.js
@@ -2,17 +2,9 @@ import Layout, {siteTitle} from '../components/layout'
 import cv from '../styles/cv.module.scss'
 
 export default function Cv() {
-  if (typeof window !== 'undefined' && document.documentElement.clientWidth >= 880) {
-    return (
-      <Layout cv>
-        <iframe src="https://sebibasti.github.io/" className={cv.iframe} frameBorder="0" width="100%" height="100%"/>
-      </Layout>
-    )
-  } else {
-    return (
-      <Layout cv>
-        <iframe src="/documents/cv_sebastian_remm.pdf" className={cv.iframe} frameBorder="0" width="100%" height="100%"/>
-      </Layout>
-    )
-  }
+  return (
+    <Layout cv>
+      <iframe src="/documents/cv_sebastian_remm.pdf" className={cv.iframe} frameBorder="0" width="100%" height="100%"/>
+    </Layout>
+  )
 }
