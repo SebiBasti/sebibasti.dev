8460915d43db0e80a7c5e015d637957afc5eca50
Sebastian Remm <sebastian.remm@web.de>
2021-06-24 23:59:32 +0200

    AppleWebKit pdf monkey patch

--- pages/cv.js
+++ pages/cv.js
@@ -2,9 +2,17 @@ import Layout, {siteTitle} from '../components/layout'
 import cv from '../styles/cv.module.scss'
 
 export default function Cv() {
-  return (
-    <Layout cv>
-      <iframe src="/documents/cv_sebastian_remm.pdf" className={cv.iframe} frameBorder="0" width="100%" height="100%"/>
-    </Layout>
-  )
+  if (navigator.userAgent.includes('AppleWebKit')) {
+    return (
+      <Layout cv>
+        <iframe src="https://drive.google.com/viewerng/viewer?embedded=true&url=sebibasti-dev.vercel.app/documents/cv_sebastian_remm.pdf" className={cv.iframe} frameBorder="0" width="100%" height="100%"/>
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout cv>
+        <iframe src="/documents/cv_sebastian_remm.pdf" className={cv.iframe} frameBorder="0" width="100%" height="100%"/>
+      </Layout>
+    )
+  }
 }
