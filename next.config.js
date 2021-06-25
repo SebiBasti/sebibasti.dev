a46b9bc52e1150fa332148755c1e7c319abb3af8
Sebastian Remm <sebastian.remm@web.de>
2021-06-25 15:44:17 +0200

    ok, I don't understand csp

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/cv',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: 'script-src self https://apis.google.com',
-          },
-        ],
-      },
-    ]
-  },
-}
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: '/cv',
+//         headers: [
+//           {
+//             key: 'Content-Security-Policy',
+//             value: 'script-src self https://apis.google.com',
+//           },
+//         ],
+//       },
+//     ]
+//   },
+// }
+
+// TODO: learn CSP
 
 // source: https://www.cspisawesome.com/content_security_policies
