1ef5e41dc7ce78ea7c22b15abecac3f9f0a93531
Sebastian Remm <sebastian.remm@web.de>
2021-07-08 15:58:36 +0200

    deactivated blog and about me link

--- components/navbar.js
+++ components/navbar.js
@@ -9,16 +9,16 @@ export default function Navbar() {
       <input type="checkbox" id="nav-toggle" className={navbar.toggle}/>
       <nav className={navbar.nav}>
         <ul>
-          <li>
-            <Link href="/about">
-              <a>about me</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              <a>blog</a>
-            </Link>
-          </li>
+          {/*<li>*/}
+          {/*  <Link href="/about">*/}
+          {/*    <a>about me</a>*/}
+          {/*  </Link>*/}
+          {/*</li>*/}
+          {/*<li>*/}
+          {/*  <Link href="/blog">*/}
+          {/*    <a>blog</a>*/}
+          {/*  </Link>*/}
+          {/*</li>*/}
           <li>
             <Link href="/cv">
               <a>cv</a>
