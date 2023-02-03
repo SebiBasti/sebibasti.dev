864468dbe0b8cf976518e0218ce0f64dfb3a9ce9
Sebastian Remm <sebastian.remm@web.de>
2023-02-03 19:01:27 +0100

    - fixing build error

--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@
 // source: https://www.cspisawesome.com/content_security_policies
 
 module.exports = {
+  target: 'serverless',
   images: {
     deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
