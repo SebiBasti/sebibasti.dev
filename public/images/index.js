2f0f0dee152fd00891c90e903e7b182250acfaea
Sebastian Remm <sebastian.remm@web.de>
2023-03-12 21:03:13 +0100

    - updated projects

--- public/images/index.js
+++ public/images/index.js
@@ -1,16 +1,16 @@
 import profilePicture from './profile.png'
-import projectPokedex from './project_pokedex.png'
+import projectWandgestaltung from './project_wandgestaltung_reinl.png'
 import projectBlogApi from './project_blog_api.png'
 
 
-projectPokedex.link = 'https://fcr78.csb.app/'
-projectPokedex.alt = 'picture project pokédex'
-projectPokedex.description = 'Pokédex in vanilla JS'
+projectWandgestaltung.link = 'https://www.wandgestaltung-reinl.de/'
+projectWandgestaltung.alt = 'Wandgestaltung Reinl Picture'
+projectWandgestaltung.description = 'website build with Next.js'
 projectBlogApi.link = 'https://github.com/SebiBasti/blog-api'
 projectBlogApi.alt = 'picture project blog api'
 projectBlogApi.description = 'Blog API with unit tests'
 
-const projectPictures = [ projectPokedex, projectBlogApi ]
+const projectPictures = [ projectWandgestaltung, projectBlogApi ]
 
 export { profilePicture }
 export { projectPictures }
