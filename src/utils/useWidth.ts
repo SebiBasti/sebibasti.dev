c2a83a155b9955b3faa0e9e8d1d34f676fb545e9
Sebastian Remm <sebastian.remm@web.de>
2023-04-03 19:17:24 +0200

    - switched from useLayoutEffect to passive-layout-effect

--- src/utils/useWidth.ts
+++ src/utils/useWidth.ts
@@ -1,7 +1,9 @@
+import useLayoutEffect from '@react-hook/passive-layout-effect'
 import useResizeObserver from '@react-hook/resize-observer'
 
-import { RefObject, useLayoutEffect, useState } from 'react'
+import { RefObject, useState } from 'react'
 
+// source: https://github.com/jaredLunde/react-hook/tree/master/packages/passive-layout-effect#readme
 export const useWidth = (target: RefObject<HTMLDivElement>) => {
   const [width, setWidth] = useState<number | undefined>(undefined)
 
