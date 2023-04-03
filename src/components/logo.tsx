35eaf38f313b965e5a94221801a576b87dfe37da
Sebastian Remm <sebastian.remm@web.de>
2023-04-03 03:28:38 +0200

    - fixed logo link

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import logo from '@/styles/logo.module.scss'
 export default function Logo() {
   return (
     <div className={logo.container}>
-      <Link href="/index">sebibasti.dev</Link>
+      <Link href="/">sebibasti.dev</Link>
     </div>
   )
 }
